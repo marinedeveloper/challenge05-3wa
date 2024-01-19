b55b3e0dd67c21ac062ec3a936e34654c87942db
marinedeveloper <marine.erbon@gmail.com>
2024-01-19 18:14:04 +0100

    WIP: tests product

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -10,8 +10,33 @@ class ProductTest extends TestCase
     protected Product $product;
 
     public function setUp():void {
-        $this->product = new Product();
+
     }
 
+    public function testGetName() {
+        $product = new Product('apple', 10.5);
+
+        $this->assertEquals('apple', $product->getName());
+    }
+
+    public function testSetName() {
+        $product = new Product('apple', 10.5);
+        $product->setName('samsung');
+
+        $this->assertEquals('samsung', $product->getName());
+    }
+
+    public function testGetPrice() {
+        $product = new Product('apple', 10.5);
+
+        $this->assertEquals(10.5, $product->getPrice());
+    }
+
+    public function testSetPrice() {
+        $product = new Product('apple', 10.5);
+        $product->setPrice( 15.5);
+
+        $this->assertEquals(15.5, $product->getPrice());
+    }
 
 }
